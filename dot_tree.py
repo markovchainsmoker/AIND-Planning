c48b84b1541f907dd12956140478fa1168df8d88
markovchainsmoker <mr.kurt.osis@gmail.com>
2018-03-19 18:13:31 +0900

    Update dot_tree.py

--- dot_tree.py
+++ dot_tree.py
@@ -1,5 +1,7 @@
 def dot_tree(problem):    
-    """    pre : a Problem() as input    post: Dot graph in string format. Paste this in http://www.webgraphviz.com/    usage: print(dot_tree(problem))    """
+"""    
+pre : a Problem() as input    
+post: Dot graph in string format. Paste this in http://www.webgraphviz.com/    usage: print(dot_tree(problem))    """
     root = Node(problem.initial)    
     flatten = lambda l: [item for sublist in l for item in sublist]
     children=[root.child_node(problem, action) for action in problem.actions(root.state)]    
