2f5f2263abcac51acfec4ea3e6041ef4e7f75cd6
markovchainsmoker <mr.kurt.osis@gmail.com>
2018-03-26 04:08:07 +0000

    Update dot_tree.py

--- dot_tree.py
+++ dot_tree.py
@@ -1,9 +1,8 @@
 def dot_tree(problem):    
-"""    
-pre : a Problem() as input    
-post: Dot graph in string format. Paste this in http://www.webgraphviz.com/ 
-usage: print(dot_tree(problem))  
-"""
+   
+#pre : a Problem() as input    
+#post: Dot graph in string format. Paste this in http://www.webgraphviz.com/ 
+#usage: print(dot_tree(problem))  
     root = Node(problem.initial)    
     flatten = lambda l: [item for sublist in l for item in sublist]
     children=[root.child_node(problem, action) for action in problem.actions(root.state)]    
@@ -11,12 +10,12 @@ usage: print(dot_tree(problem))
     greatgrandchildren=flatten([[child.child_node(problem, action) for action in problem.actions(child.state)] for child in grandchildren])    
     nodes=set(children+grandchildren+greatgrandchildren)
     import pydot    
-"""    
-    make a Dot graph of the nodes and connections    
-    initial state as double circle     
-    goal state is marked with thick lines    
-    current implementation only goes two levels down (greatgrandchildren)    
-"""            
+    
+#make a Dot graph of the nodes and connections    
+#initial state as double circle     
+#goal state is marked with thick lines    
+#current implementation only goes two levels down (greatgrandchildren)    
+            
     g = pydot.Dot()    
     g.set_type('digraph')    
     g.set_node_defaults(fontname = "helvetica",fontsize=10)    
@@ -29,5 +28,5 @@ usage: print(dot_tree(problem))
             penwidth=1                
         g.add_node(pydot.Node(name=n.state,label='{}'.format(n.state),penwidth=penwidth))    
     for n in nodes:
-        g.add_edge(pydot.Edge(n.parent.state,n.state,label='{}'.format(n.action.name)))    
+        g.add_edge(pydot.Edge(n.parent.state,n.state,label='{} ({})'.format(n.action.name,n.action.args)))    
    return g.to_string()
